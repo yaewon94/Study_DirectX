90768dd38446842a99d5b075db413a0ae7433dbd
yaewon94 <yaewon94@naver.com>
2024-10-27 14:44:38 +0900

    TreeUI :  트리노드 추가 + ERROR : TreeUI::m_root == nullptr 라 에러나는 상태 (추후 수정할것임)

--- Project/Client/TreeUI.cpp
+++ Project/Client/TreeUI.cpp
@@ -3,17 +3,86 @@
 
 TreeUI::TreeUI() 
 	: EditorUI("TreeUI")
+	, m_root(nullptr)
 {
 }
 
 TreeUI::~TreeUI()
 {
+	if (m_root != nullptr)
+	{
+		delete m_root;
+		m_root = nullptr;
+	}
 }
 
 void TreeUI::RenderUpdate()
 {
-	if (ImGui::TreeNodeEx("TestNode"), ImGuiTreeNodeFlags_Leaf)
+	m_root->RenderUpdate();
+}
+
+TreeNode* const TreeUI::AddItem(const string& name, DWORD_PTR data, TreeNode* const parent)
+{
+	TreeNode* node = new TreeNode;
+	node->m_name = name;
+	node->m_data = data;
+
+	if (parent == nullptr)
 	{
+		if (m_root == nullptr) m_root = node;
+		else throw std::logic_error("루트노드가 이미 존재합니다");
+	}
+	else
+	{
+		parent->AddChild(node);
+	}
+
+	return node;
+}
+
+TreeNode::TreeNode() 
+	: m_data(0), m_parent(nullptr)
+{
+}
+
+TreeNode::~TreeNode()
+{
+	m_data = 0;
+	m_parent = 0;
+	for (auto child : m_children)
+	{
+		if (child != nullptr)
+		{
+			delete child;
+			child = nullptr;
+		}
+	}
+}
+
+void TreeNode::RenderUpdate()
+{
+	if (ImGui::TreeNodeEx(m_name.c_str(), ImGuiTreeNodeFlags_Leaf))
+	{
+		for (auto child : m_children)
+		{
+			child->RenderUpdate();
+		}
+
 		ImGui::TreePop();
 	}
+}
+
+void TreeNode::AddChild(TreeNode* const child)
+{
+	// 중복 체크
+	for (auto _child : m_children)
+	{
+		if (child == _child)
+		{
+			throw std::logic_error("이미 존재하는 노드입니다");
+		}
+	}
+
+	m_children.push_back(child);
+	child->m_parent = this;
 }
--- Project/Client/TreeUI.h
+++ Project/Client/TreeUI.h
@@ -1,8 +1,31 @@
 #pragma once
 #include "EditorUI.h"
 
+// 트리노드
+struct TreeNode
+{
+	friend class TreeUI;
+
+private:
+	string m_name;
+	DWORD_PTR m_data;
+
+	TreeNode* m_parent;
+	vector<TreeNode*> m_children;
+
+	TreeNode();
+	~TreeNode();
+
+	void RenderUpdate();
+	void AddChild(TreeNode* const child);
+};
+
+// 트리 형태의 UI
 class TreeUI final : public EditorUI
 {
+private:
+	TreeNode* m_root;
+
 public:
 	TreeUI();
 	~TreeUI();
@@ -12,4 +35,5 @@ public:
 
 public:
 	virtual EDITOR_UI_TYPE GetType() final { return EDITOR_UI_TYPE::TREE; }
+	TreeNode* const AddItem(const string& name, DWORD_PTR data, TreeNode* const parent = nullptr);
 };
